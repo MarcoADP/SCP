f8121919ea7e47ec341b4fa398525e1fa2af3b33
guest-iB04bP <guest-iB04bP@DINSL007-PC08>
2016-01-27 11:12:53 -0200

    começo Leitura

--- SCP/src/scp/SCP.java
+++ SCP/src/scp/SCP.java
@@ -1,21 +1,57 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package scp;
 
-/**
- *
- * @author guest-iB04bP
- */
-public class SCP {
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public class SCP {
+    public static int buscaNum(String linha){
+        int indice = linha.indexOf(" ");
+        String sLinha = linha.substring(indice);
+        int nLinha = Integer.parseInt(sLinha.trim());
+        return nLinha;
+    }
+    
+    
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        
+        String arq = "teste2.txt";
+        FileInputStream stream = new FileInputStream(arq);
+        InputStreamReader reader = new InputStreamReader(stream);
+        //InputStreamReader reader = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(reader);
+        
+        String linha = br.readLine();
+        String coluna = br.readLine();
+        String lixo = br.readLine();
+        int nLinha = buscaNum(linha);      
+        int nColuna = buscaNum(coluna);
+        
+        coluna = br.readLine();
+        while(!coluna.equals("")){
+            int indice = coluna.indexOf(" ");
+            String linhaAtual = coluna.substring(indice);
+            linhaAtual = linhaAtual.trim();
+            String elemento[];
+            elemento = linhaAtual.split(" ");
+            int k = 0;
+            String elAtual = elemento[k];
+            while(!elAtual.equals("")){
+                System.out.println(elAtual);
+                k++;
+                elAtual = elemento[k];
+            }
+            System.out.println("======");
+            coluna = br.readLine();
+        }
+        //System.out.println(nLinha);
+        //System.out.println(coluna);
+        
+        
+        
+        
     }
     
 }
