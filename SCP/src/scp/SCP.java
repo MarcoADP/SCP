816b4ce23ee9950a21f652073f1642a53911ffb3
Marco Aurélio Paulino <marco23_aurelio@hotmail.com>
2016-01-30 17:36:17 -0200

    Leitura feita

--- SCP/src/scp/SCP.java
+++ SCP/src/scp/SCP.java
@@ -5,19 +5,26 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class SCP {
     public static int buscaNum(String linha){
+        //System.out.println(linha);
         int indice = linha.indexOf(" ");
         String sLinha = linha.substring(indice);
-        int nLinha = Integer.parseInt(sLinha.trim());
+        int nLinha = Integer.parseInt(sLinha.trim()); 
+        //System.out.println(nLinha);
         return nLinha;
     }
     
     
     public static void main(String[] args) throws FileNotFoundException, IOException {
         
-        String arq = "teste2.txt";
+        //Estrturas Utilizadas
+        ArrayList<Double> listaPeso = new ArrayList<>();
+        
+        String arq = "teste8.txt";
         FileInputStream stream = new FileInputStream(arq);
         InputStreamReader reader = new InputStreamReader(stream);
         //InputStreamReader reader = new InputStreamReader(System.in);
@@ -25,29 +32,37 @@ public class SCP {
         
         String linha = br.readLine();
         String coluna = br.readLine();
-        String lixo = br.readLine();
         int nLinha = buscaNum(linha);      
         int nColuna = buscaNum(coluna);
+        br.readLine(); //Lendo a linha dados ou densidades
         
-        coluna = br.readLine();
-        while(!coluna.equals("")){
-            int indice = coluna.indexOf(" ");
-            String linhaAtual = coluna.substring(indice);
-            linhaAtual = linhaAtual.trim();
-            String elemento[];
-            elemento = linhaAtual.split(" ");
-            int k = 0;
-            String elAtual = elemento[k];
-            while(!elAtual.equals("")){
-                System.out.println(elAtual);
-                k++;
-                elAtual = elemento[k];
+        int i = 0;
+        linha = br.readLine();
+        while(!linha.equals("")){
+            int id = 0;
+            String sublinha[] = linha.split(" ");
+            for (String sublinha1 : sublinha) {
+                if(!sublinha1.equals("")){
+                    if(id == 0){
+                        System.out.print("coluna = " + sublinha1 + " -- ");
+                        id++;
+                    } else if(id == 1){
+                        listaPeso.add(Double.parseDouble(sublinha1));
+                        id++;
+                    } else { 
+                        System.out.print(sublinha1 + " ");
+                    }
+                }
+                /*if(!sublinha1.equals("")){
+                    System.out.print(sublinha1 + " ");
+                }*/
             }
-            System.out.println("======");
-            coluna = br.readLine();
+            linha = br.readLine();
+            System.out.println();
         }
+        //System.out.println(listaPeso.get(4960));
         //System.out.println(nLinha);
-        //System.out.println(coluna);
+        //System.out.println(nColuna);
         
         
         
