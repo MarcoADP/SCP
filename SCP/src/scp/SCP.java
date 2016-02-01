3641b740abf427951721f1e519bf6c08d52df942
Marco Aurélio Paulino <marco23_aurelio@hotmail.com>
2016-02-01 11:09:48 -0200

    criado classe cromossomo e listaColuna

--- SCP/src/scp/SCP.java
+++ SCP/src/scp/SCP.java
@@ -6,9 +6,27 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class SCP {
+    public class Cromossomo {
+        ArrayList<Integer> listaElementos = new ArrayList<>(); 
+        Double custoTotal;
+
+        public Cromossomo() {
+            listaElementos = new ArrayList<>();
+            custoTotal = 0.0;
+        }
+        
+        public Cromossomo(ArrayList<Integer> lista, ArrayList<Double> peso){
+            this.listaElementos = lista;
+            this.custoTotal = 0.0;
+            for(int a : lista){
+                this.custoTotal += peso.get(a-1);
+            }
+        }
+        
+    }
+    
     public static int buscaNum(String linha){
         //System.out.println(linha);
         int indice = linha.indexOf(" ");
@@ -21,10 +39,14 @@ public class SCP {
     
     public static void main(String[] args) throws FileNotFoundException, IOException {
         
+        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
         //Estrturas Utilizadas
         ArrayList<Double> listaPeso = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> listaColuna = new ArrayList<>();
+        ArrayList<Cromossomo> listaPopulacao = new ArrayList<>(); 
         
-        String arq = "teste8.txt";
+        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        String arq = "teste2.txt";
         FileInputStream stream = new FileInputStream(arq);
         InputStreamReader reader = new InputStreamReader(stream);
         //InputStreamReader reader = new InputStreamReader(System.in);
@@ -41,28 +63,44 @@ public class SCP {
         while(!linha.equals("")){
             int id = 0;
             String sublinha[] = linha.split(" ");
+            listaColuna.add(new ArrayList<>());
             for (String sublinha1 : sublinha) {
+                ArrayList<Integer> aux = new ArrayList<>();
+                //System.out.println(sublinha1);
                 if(!sublinha1.equals("")){
                     if(id == 0){
-                        System.out.print("coluna = " + sublinha1 + " -- ");
+                        //System.out.print("coluna = " + sublinha1 + " -- ");
                         id++;
                     } else if(id == 1){
                         listaPeso.add(Double.parseDouble(sublinha1));
                         id++;
                     } else { 
-                        System.out.print(sublinha1 + " ");
+                        //System.out.print(sublinha1 + " ");
+                        //aux.add(Integer.parseInt(sublinha1));
+                        listaColuna.get(listaColuna.size() - 1).add(Integer.parseInt(sublinha1));
+                        //System.out.println(listaColuna.get(listaColuna.size() - 1));
+                        //System.out.print(aux);
                     }
                 }
-                /*if(!sublinha1.equals("")){
-                    System.out.print(sublinha1 + " ");
-                }*/
+                
             }
+            
             linha = br.readLine();
-            System.out.println();
+            //System.out.println();
         }
+        
+        /*int kk = 0;
+        for(ArrayList a : listaColuna){
+            System.out.print(++kk + " --> ");
+            for(int j  = 0; j < a.size(); j++){
+                System.out.print(a.get(j) + " - ");
+            }
+            System.out.println();
+        }*/
         //System.out.println(listaPeso.get(4960));
         //System.out.println(nLinha);
         //System.out.println(nColuna);
+        
         
         
         
