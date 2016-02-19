a3d0976b357dc0f2f3abd6acf0b6f719a706a4d4
Marco Aurélio Paulino <marco23_aurelio@hotmail.com>
2016-02-19 13:49:36 -0200

    DONE: Classificao para Crossover

--- SCP/src/scp/Algoritmo.java
+++ SCP/src/scp/Algoritmo.java
@@ -6,6 +6,7 @@ public class Algoritmo {
     public static void algoritmoGenetico(ArrayList<Double> listaPeso, ArrayList<ArrayList<Integer>> listaColuna, ArrayList<Integer>[] listaLinha, int qtdPop, int nLinha){
         //Geração de População
         Populacao populacao = new Populacao(nLinha, qtdPop, listaColuna, listaLinha, listaPeso);
-        
+        Cromossomo cr[] = populacao.classificaPopulacao();
+        System.out.println(cr[0].custoTotal + " --- " + cr[1].custoTotal);
     }
 }
